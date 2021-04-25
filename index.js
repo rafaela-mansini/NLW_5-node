b9ac3b22f3dfc1fe1dcd514314dd9e208b968a8b
rafaela-mansini <rafaela_mansini@hotmail.com>
2021-04-25 18:05:06 -0300

    config: Docker compose configurations

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Hi Docker!!!");
+  res.send("Hi Docker with docker-compose!!!");
 });
 
 app.listen(3000);
