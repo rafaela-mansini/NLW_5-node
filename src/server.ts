e0e37cb7a26bc118c5ab1fe355d7385840db1b7d
rafaela-mansini <rafaela_mansini@hotmail.com>
2021-04-26 19:30:49 -0300

    feat: create get and post method from a test

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,13 @@
-import express from 'express';
+import express, { request, response } from 'express';
 
 const app = express();
+
+app.get('/', (request, response) => {
+    return response.json({message: "Hello NLW #05"})
+});
+
+app.post('/', (request, response) => {
+    return response.json({message: "User was saved."});
+});
 
 app.listen(3000, () => console.log("Server running on port 3000"));
