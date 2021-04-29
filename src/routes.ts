ca1c1bdda427c1277f2793180f5e33e0b54c8c3b
rafaela-mansini <rafaela_mansini@hotmail.com>
2021-04-29 20:40:49 -0300

    refactor: separated create method into a controller file

--- src/routes.ts
+++ src/routes.ts
@@ -1,24 +1,9 @@
-import { response, Router } from "express";
-import { getCustomRepository } from "typeorm";
-import { SettingsRepository } from "./repositories/SettingsRepository";
+import { SettingsController } from "./controllers/SettingsController";
+import { Router } from "express";
 
 const routes = Router();
 
-/**
- * Param types
- * Route params => params from routes;
- * Query params => usually from searches and filters, passing by a url;
- * Body params => sending with json format
- */
-
-routes.post("/settings", async (req, res) => {
-    const settingsRepository = getCustomRepository(SettingsRepository);
-    const { chat, username } = req.body;
-
-    const settings = settingsRepository.create({ chat, username });
-    await settingsRepository.save(settings);
-
-    return res.json(settings);
-});
+const settingsController = new SettingsController();
+routes.post("/settings", settingsController.create);
 
 export { routes };
