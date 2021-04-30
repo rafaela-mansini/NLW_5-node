4567971aa1560a0d25dc05a0e92576d146da30c0
rafaela-mansini <rafaela_mansini@hotmail.com>
2021-04-29 21:12:52 -0300

    refactor: abstracting the business rules for the services layer

--- src/controllers/SettingsController.ts
+++ src/controllers/SettingsController.ts
@@ -1,16 +1,17 @@
 import { Request, Response } from "express";
-import { getCustomRepository } from "typeorm";
-import { SettingsRepository } from "../repositories/SettingsRepository";
+import { SettingsService } from "../services/SettingsService";
 
 class SettingsController {
     async create(req: Request, res: Response){
-        const settingsRepository = getCustomRepository(SettingsRepository);
         const { chat, username } = req.body;
-    
-        const settings = settingsRepository.create({ chat, username });
-        await settingsRepository.save(settings);
-    
-        return res.json(settings);
+        const settingsService = new SettingsService();
+
+        try {
+            const settings = await settingsService.create({ chat, username });
+            return res.json(settings);            
+        } catch (error) {
+            return res.status(400).json({ message: error.message });
+        }
     }
 }
 
