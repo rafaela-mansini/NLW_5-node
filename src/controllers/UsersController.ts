d95f1e493353adbaafbd3a54a595a43b7db58a77
rafaela-mansini <rafaela_mansini@hotmail.com>
2021-05-02 17:14:36 -0300

    feat: store users

---
+++ src/controllers/UsersController.ts
@@ -0,0 +1,14 @@
+import { Request, Response } from "express";
+import { UsersService } from "../services/UsersService";
+
+class UsersController {
+    async create(req: Request, res: Response): Promise<Response>{
+        const { email } = req.body;
+        const userService = new UsersService();
+
+        const user = await userService.create(email);
+        return res.status(200).json(user);
+    }
+}
+
+export { UsersController };
