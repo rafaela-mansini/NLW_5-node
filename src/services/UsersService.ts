2972e97ca29045dc6ebe2d9e0c43059be4f87d80
rafaela-mansini <rafaela_mansini@hotmail.com>
2021-05-02 18:26:43 -0300

    feat: inserting repository with a private attribute in services

--- src/services/UsersService.ts
+++ src/services/UsersService.ts
@@ -1,17 +1,23 @@
-import { getCustomRepository } from "typeorm";
+import { getCustomRepository, Repository } from "typeorm";
+import { User } from "../entities/User";
 import { UsersRepository } from "../repositories/UsersRepository";
 
 class UsersService {
+    private userRepository: Repository<User>
+
+    constructor(){
+        this.userRepository = getCustomRepository(UsersRepository);
+    }
+
     async create(email: string){
-        const userRepository = getCustomRepository(UsersRepository);
-        const userExist = await userRepository.findOne({ email });
+        const userExist = await this.userRepository.findOne({ email });
 
         if(userExist){
             return userExist;
         }
 
-        const user = userRepository.create({ email });
-        await userRepository.save(user);
+        const user = this.userRepository.create({ email });
+        await this.userRepository.save(user);
 
         return user;
     }
